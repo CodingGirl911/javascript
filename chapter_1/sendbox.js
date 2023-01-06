fef667f26b8dac5119373d33ebee565eca52c668
nino <ninonazgaidze2@gmail.com>
2023-01-06 16:01:54 +0400

    first change

---
+++ chapter_1/sendbox.js
@@ -0,0 +1,72 @@
+//const title = 'best reads of 2019';
+//const author = 'Mario';
+//const likes = 30;
+
+//concatenation way
+//let result = 'the blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
+//console.log (result);
+
+//tempalte string
+//let result = `The Blog Called ${title} by ${author} has ${likes} likes; `
+//console.log (result);
+
+
+//creating html templates
+//let html =`
+//<h2>${title}
+//<p> by ${author} <p>
+//<span> this blog has ${likes} likes <span>
+//`;
+//console.log (html);
+
+//let mura = 'beautifull dog';
+//console.log (mura);
+
+//const name = Mura;
+//const paragraph = story;
+//const author = nino;
+
+//let realstory = `I have written a ${paragraph} on a real life of a + ${name} author is me ${author};`
+//console.log (realstory);
+
+//let ninjas = ['shaun', 'ryu', 'chun-li'];
+
+//ninjas [1];
+//console.log (ninjas[1]);
+
+//let random = ['shaun', 'crystal', 30, 40];
+//console.log(random);
+
+
+//.log(ninjas.length);
+
+//array methods
+
+//let result = ninjas.join('-');
+//console.log(result);
+
+//boolians & comparisons
+
+console.log(true, false, "true", "false");
+
+//methods can return booleans
+
+let email= 'luigi@thenetninja.co.uk';
+let names = ['mario', 'luigi', 'toad']
+
+//let result = email.includes('!');
+//let result = names.includes ('nino')
+
+
+//console.log(result);
+
+//comparison operators
+let age = 25;
+
+console.log ( age == 25);
+console.log ( age == 30);
+console.log ( age != 30);
+console.log ( age < 20);
+console.log ( age < 20);
+console.log ( age > 26);
+
