56e5c02622e58f3a47868b1880dca7c413e94ff4
nino <ninonazgaidze2@gmail.com>
2023-01-08 16:52:03 +0400

    console log

--- chapter_1/sendbox.js
+++ chapter_1/sendbox.js
@@ -47,12 +47,12 @@
 
 //boolians & comparisons
 
-console.log(true, false, "true", "false");
+//console.log(true, false, "true", "false");
 
 //methods can return booleans
 
-let email= 'luigi@thenetninja.co.uk';
-let names = ['mario', 'luigi', 'toad']
+//let email= 'luigi@thenetninja.co.uk';
+//let names = ['mario', 'luigi', 'toad']
 
 //let result = email.includes('!');
 //let result = names.includes ('nino')
@@ -63,10 +63,42 @@ let names = ['mario', 'luigi', 'toad']
 //comparison operators
 let age = 25;
 
-console.log ( age == 25);
-console.log ( age == 30);
-console.log ( age != 30);
-console.log ( age < 20);
-console.log ( age < 20);
-console.log ( age > 26);
+//console.log ( age == 25);
+//console.log ( age == 30);
+//console.log ( age != 30);
+//console.log ( age < 20);
+//console.log ( age <= 20);
+//console.log ( age >= 26);
 
+//let name = 'shaun';
+//console.log (name == 'shaun');
+//console.log (name == 'Shaun');
+//console.log (name > 'Crystal');
+//console.log (name > 'crystal');
+//console.log (name > 'Shaun');
+//console.log (name > 'Crystal');
+
+//console.log(age != 25);
+//console.log(age != '25');
+
+
+//strict comparison (different types can not be equal)
+ //console.log(age === 25)
+//console.log(age ==='25')
+
+
+//type conversion
+let score = '100';
+
+//score = Number(score);
+//console.log (score + 1);
+//console.log(typeof score); //sitkvebit dawers ricxvs
+
+//let result = Number('hello');
+//let result = String(50);
+//let result = Boolean(50)
+//let result = Boolean(0)
+let result = Boolean ('0')
+console.log (result, typeof result);
+
+console.log (result);
